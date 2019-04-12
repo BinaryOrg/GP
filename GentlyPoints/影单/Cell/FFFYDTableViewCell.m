4b3198a7de359d51c70adf8378756bd34f8af194
GodzzZZZ <godzzzzz@qq.com>
2019-04-12 21:02:09 +0800

    l

--- GentlyPoints/影单/Cell/FFFYDTableViewCell.m
+++ GentlyPoints/影单/Cell/FFFYDTableViewCell.m
@@ -95,7 +95,7 @@ UICollectionViewDataSource
             [cell.poster yy_setImageWithURL:[NSURL URLWithString:self.yd[indexPath.row].poster] placeholder:[UIImage imageNamed:@""] options:(YYWebImageOptionProgressiveBlur|YYWebImageOptionProgressive) completion:nil];
         }else {
             cell.nameLabel.text = @"";
-            [cell.poster yy_setImageWithURL:[NSURL URLWithString:@""] placeholder:[UIImage imageNamed:@"custom_all_108x154_"] options:(YYWebImageOptionProgressiveBlur|YYWebImageOptionProgressive) completion:nil];
+            [cell.poster yy_setImageWithURL:[NSURL URLWithString:@""] placeholder:[UIImage imageNamed:@"movie_discover_92x134_"] options:(YYWebImageOptionProgressiveBlur|YYWebImageOptionProgressive) completion:nil];
         }
     }
     
