f8853dafa92b40a6e732d10963e4ab584e4fb32d
Maker <153751320@qq.com>
2019-04-14 11:57:24 +0800

    aa
    aa

---
+++ GentlyPoints/Mine/View/GPMineHeaderView.h
@@ -0,0 +1,20 @@
+//
+//  GPMineHeaderView.h
+//  GentlyPoints
+//
+//  Created by Maker on 2019/4/13.
+//  Copyright © 2019 MakerYang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GPMineHeaderView : UIView
+
+/** <#class#> */
+@property (nonatomic, strong) GODUserModel *model;
+
+@end
+
+NS_ASSUME_NONNULL_END
